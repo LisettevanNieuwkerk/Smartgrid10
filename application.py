ba733a7a8a40e49128c5fb384c39dd7be57953ff
davidmokken <davidmokken>
2019-04-09 13:02:26 +0200

    Updated the main application, we still need to find a way to split the text file and put it in the dict

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from house import House
 from battery import Battery
-from neighbourhood import neighbourhood
+from neighbourhood import Neighbourhood
 import sys
-import cs50
+import csv
 
 class SmartGrid():
     """
@@ -14,9 +14,9 @@ class SmartGrid():
         """
         Create houses and batteries for the problem.
         """
-        self.houses = self.load_houses(f"wijk{neighbourhood_name}_huizen.csv")
-        self.batteries = self.load.batteries(f"C:\Users\David\Documents\Minor Programmeren\Heuristics\wijk{neighbourhood_name}_batterijen.txt")
-        self.inventory = Inventory()        
+        # self.houses = self.load_houses(f"wijk{neighbourhood_name}_huizen.csv")
+        self.batteries = self.load_batteries(f"wijk{neighbourhood_name}_batterijen.txt")
+        # self.inventory = Inventory()        
 
     def load_houses(self, filename):
         """
@@ -28,6 +28,7 @@ class SmartGrid():
         houses = dict()
         with open(filename, 'r') as infile:
             reader = csv.reader(infile)
+            next(reader)
             id = 0
             for rows in reader:
                 xpos = int(rows[0])
@@ -38,15 +39,27 @@ class SmartGrid():
                 houses[id] = house
 
      
-    def load_battery(self, filename):
+    def load_batteries(self, filename):
         """
         Load batteries from filename.
         Return a dictionairt of 'id': Battery objects.
         """
         # First we parse all the data we need to create the houses with.
         # All parsed lines of data are saved to houses_data.  
-        battaries = dict()
+        batteries = dict()
         with open(filename, "r") as infile:
+            next(infile)
+            id = 0
+            for rows in infile:
+                pos = rows[0]
+                capacity = rows[1]
+                print(pos)
+                print(capacity)
+                
+                # id += 1
+                # battery = Battery(id, xpos, ypos, capacity)
+                # batteries[id] = battery 
 
 
-        #split
+
+SmartGrid(1)

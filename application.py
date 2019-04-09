0c82a11553a2bd43b300c623c330e9d00075da29
LisettevanNieuwkerk <lisettevannieuwkerk@gmail.com>
2019-04-09 16:51:03 +0200

    load data and calculate distance

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from house import House
 from battery import Battery
-from neighbourhood import Neighbourhood
 import sys
 import csv
 
@@ -14,52 +13,105 @@ class SmartGrid():
         """
         Create houses and batteries for the problem.
         """
-        # self.houses = self.load_houses(f"wijk{neighbourhood_name}_huizen.csv")
+        self.houses = self.load_houses(f"wijk{neighbourhood_name}_huizen.csv")
         self.batteries = self.load_batteries(f"wijk{neighbourhood_name}_batterijen.txt")
-        # self.inventory = Inventory()        
+        # self.inventory = Inventory()
 
     def load_houses(self, filename):
         """
-        Load houses from filename. 
+        Load houses from filename.
         Return a dictionary of 'id' : House objects.
         """
         # First we parse all the data we need to create the houses with.
         # All parsed lines of data are saved to houses_data.
-        houses = dict()
+        houses = {}
         with open(filename, 'r') as infile:
             reader = csv.reader(infile)
+            # Skip first row
             next(reader)
-            id = 0
+            # Set count for houses
+            id = 1
+            # Iterate over every row in file
             for rows in reader:
+                # Put id, x and y position, max output into house object
                 xpos = int(rows[0])
                 ypos = int(rows[1])
                 max_output = float(rows[2])
-                id += 1
                 house = House(id, xpos, ypos, max_output)
+                # Add house to dict with id as key
                 houses[id] = house
+                id += 1
 
-     
+        return houses
+
+
     def load_batteries(self, filename):
         """
         Load batteries from filename.
         Return a dictionairt of 'id': Battery objects.
         """
         # First we parse all the data we need to create the houses with.
-        # All parsed lines of data are saved to houses_data.  
-        batteries = dict()
+        # All parsed lines of data are saved to houses_data.
+        batteries = {}
         with open(filename, "r") as infile:
             next(infile)
-            id = 0
-            for rows in infile:
-                pos = rows[0]
-                capacity = rows[1]
-                print(pos)
-                print(capacity)
-                
-                # id += 1
-                # battery = Battery(id, xpos, ypos, capacity)
-                # batteries[id] = battery 
+            id = 1
+            for line in infile:
+                # Filter chars out of line
+                line = line.replace('[', '').replace(']', '').replace(',', '').replace('\t\t', ' ').replace('\t', ' ').replace('\n', '')
+                firstspaceFound = False
+                firstSpace = None
+                secondSpace = None
+                # Go over index of line
+                for i in range(len(line)):
+                    # Check for space and save index
+                    if (line[i] == ' '):
+                        if (firstspaceFound == False):
+                            firstSpace = i
+                            firstspaceFound = True
+                        if (firstspaceFound == True):
+                            secondSpace = i
+                # Set values for battery
+                xpos = int(line[:firstSpace])
+                ypos = int(line[firstSpace+1:secondSpace])
+                capacity = float(line[secondSpace+1:])
+                # Create battery object and put in dict with id as key
+                battery = Battery(id, xpos, ypos, capacity)
+                batteries[id] = battery
+                id += 1
+
+        return batteries
+
+    def play(self):
+        # Print dictionary of bateries
+        #for i in self.batteries:
+        #    print(self.batteries[i])
+
+        # Print dictionary of houses
+        #for i in self.houses:
+        #    print(self.houses[i])
+
+        xdist = 0
+        ydist = 0
+        kabels = {}
+        # Iterate over every house
+        for keyh in self.houses:
+            # Put house in dict
+            house = f"house_{keyh}"
+            kabels[house] = {}
+            # Itirate over every battery
+            for keyb in self.batteries:
+                # Calculate manhattan distance from house to battery
+                xdist = abs(self.houses[keyh].xpos - self.batteries[keyb].xpos)
+                ydist = abs(self.houses[keyh].ypos - self.batteries[keyb].ypos)
+                manhattandist = xdist + ydist
+                # Set battery as key in list of house and add distance
+                battery = f"battery_{keyb}"
+                kabels[house][battery] = manhattandist
+        # Dict with possible distances
+        print(kabels)
 
 
-
-SmartGrid(1)
+if __name__ == "__main__":
+    smartgrid = SmartGrid(1)
+    smartgrid.play()

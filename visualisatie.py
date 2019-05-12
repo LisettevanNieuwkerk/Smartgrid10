d057dab9648c4b889dce5d57f45f1f2d751eb02f
davidmokken <davidmokken@hotmail.com>
2019-05-12 14:00:01 +0200

    Visualizer

--- visualisatie.py
+++ visualisatie.py
@@ -29,50 +29,63 @@ def load_results(filename):
     data = pd.read_csv(filename)
 
     # Cleans the data
-    data = data[['Iteration', 'Value']]
-    data['Iteration'] = pd.to_numeric(data['Iteration'])
-    data['Value'] = pd.to_float(data['Value'])
+    data = data[['Run', 'Total Distance', 'Costs Grid', 'Costs Batteries', 'Total Costs']]
+    data['Runs'] = pd.to_numeric(data['Run'])
+    data['Total Distance'] = pd.to_numeric(data['Total Distance'])
+    data['Costs Grid'] = pd.to_numeric(data['Costs Grid'])
+    data['Costs Batteries'] = pd.to_numeric(data['Costs Batteries'])
+    data['Total Costs'] = pd.to_numeric(data['Total Costs'])
 
     return data
 
-def plot_scatter(iteration, value):
+def plot_scatter(runs, costs):
     """
     Plots a scatterplot of the inserted data
     """
     # Forms the scatterplot
-    plt.scatter(iteration, value)
+    plt.scatter(runs, costs)
 
     # Adds a title and axis names
-    plt.title('Value change in iterations', fontweight='bold')
-    plt.xlabel('Iterations')
-    plt.ylabel('Value')
+    plt.title('Costs change in runs', fontweight='bold')
+    plt.xlabel('Runs')
+    plt.ylabel('Costs')
     plt.ylim(2000, 6000)
     plt.grid(True)
     
     # Actually shows the scatterplot
     plt.show()
 
-def plot_line(iteration, value):
+def plot_line(runs, costs):
     """
     plots a histogram of the insterted data
     """
     # Forms the histogram
-    plt.plot(iteration, value, 'k', iteration, value, 'go')
+    plt.plot(runs, costs, 'k', runs, costs, 'go')
 
     # Adds the title and axis names
-    plt.title('Value change in iterations', fontweight='bold')
-    plt.xlabel('Iterations')
-    plt.ylabel('Value')
+    plt.title('Costs change in runs', fontweight='bold')
+    plt.xlabel('Runs')
+    plt.ylabel('Costs')
     plt.ylim(2000, 6000)
     plt.grid(True)
 
     # Actually shows the histogram
     plt.show
 
+def write_to_csv(self, total_distance, costs_grid, costs_batteries, total_costs):
+    """
+    Appends result to an existing csv file
+    """
+    with open('results_greedy_csv', 'a') as infile:
+        fields = ['Total Distance', 'Costs Grid', 'Costs Batteries', 'Total Costs']
+        writer = csv.writer(infile, fieldnames=fields)
+        writer.writeheader() 
+        input = [str(total_distance), str(costs_grid), str(costs_batteries), str(total_costs)]
+        writer.writerow(input)
 
 # if __name__ == '__main__':
 #     data = load_results('CSV FILE')
-#     value = data(data['value'])
-#     iterations = data(data['iterations'])
-#     plot_scatter(value, iterations)
-#     plot_line(value, iterations)
+#     value = data(data['Value'])
+#     runs = data(data['Runs'])
+#     plot_scatter(value, runs)
+#     plot_line(value, runs)
